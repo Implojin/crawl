b8575419244a525c7a5380276934177feb5251cb
Implojin <implosionwow@gmail.com>
2025-10-17 23:05:25 -0500

    ci: bump more runs to ubuntu-latest
    The android build was just missing tiles dependencies here.
    
    Under mingw, crosscompiling the contrib version of libpng didn't make it
    available during the build, where it was needed to generate tilesheets.
    (See also a53296f351.)
    
    The source packages run was overlooked in c80b890e59, so here we bump
    that too.

--- .github/workflows/deps.py
+++ .github/workflows/deps.py
@@ -60,7 +60,7 @@ def _packages_to_install(args: argparse.Namespace) -> Set[str]:
         "ccache",
         "advancecomp",  # used to compress release zips and png sprite sheets
     }
-    if "TILES" in args.build_opts or "WEBTILES" in args.build_opts:
+    if "TILES" in args.build_opts or "WEBTILES" in args.build_opts or "ANDROID" in args.build_opts:
         packages.update(
             [
                 "libsdl2-image-dev",
@@ -78,6 +78,9 @@ def _packages_to_install(args: argparse.Namespace) -> Set[str]:
     if args.crosscompile:
         packages.add("mingw-w64")
         packages.add("nsis")  # makensis used to build Windows installer
+        if "tiles" in args.build_type:
+            # native libpng used to generate tilesheets during the build
+            packages.add("libpng-dev")
     if args.compiler == "clang":
         # dependencies for llvm.sh
         packages.update(["lsb-release", "wget", "software-properties-common"])
